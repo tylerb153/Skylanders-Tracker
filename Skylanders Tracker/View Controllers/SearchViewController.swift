8835412b562ec57010f633e0ed5a6b5c05cb3e3b
Tyler Bischoff <tbisch1@live.spcollege.edu>
2022-08-10 21:21:50 -0400

    Search Page Functioning with a rudimentary search.

--- Skylanders Tracker/View Controllers/SearchViewController.swift
+++ Skylanders Tracker/View Controllers/SearchViewController.swift
@@ -6,10 +6,108 @@
 //
 
 import UIKit
+import CoreData
 
 class SearchViewController: UIViewController {
-
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var skylandersList: [NSManagedObject] = []
+    var skylandersToDisplay: [NSManagedObject]{ getSkylandersToDisplay()}
+    var skylandersCount: Int{skylandersToDisplay.count}
+    
+    var skylanderToSend: NSManagedObject?
+    var searchText: String = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.becomeFirstResponder()
+        refreshData()
+//        print(skylandersList)
+//        print(skylandersCount)
+        
+        let cellNib = UINib(nibName: "SkylanderCell", bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: "SkylanderCell")
+        
+        tableView.reloadData()
+//        print("3")
+    }
+    
+// MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "DisplaySkylander" {
+            let SkylandersDetailViewController = segue.destination as! SkylandersDetailViewController
+            SkylandersDetailViewController.chosenSkylander = skylanderToSend
+        }
+    }
+
+// MARK: - Data Processing
+    func refreshData() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Skylander")
+        do {
+            skylandersList = try managedContext.fetch(fetchRequest)
+        }
+        catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+//        print(skylandersList)
+    }
+    
+    func getSkylandersToDisplay() -> [NSManagedObject] {
+        var skylandersToDisplay: [NSManagedObject] = []
+        for skylander in skylandersList{
+            let skylanderName = skylander.value(forKey: "name") as! String
+            if searchText != ""{
+                if skylanderName.uppercased().contains(searchText.uppercased()) {
+                    skylandersToDisplay.append(skylander)
+                }
+            }
+            else {
+                skylandersToDisplay.append(skylander)
+            }
+        }
+//        print(skylandersToDisplay)
+        return skylandersToDisplay
+    }
+}
+
+// MARK: - tableView Delegate
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        print(skylandersCount)
+        return skylandersCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "SkylanderCell"
+        tableView.rowHeight = 66
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as! SkylanderCell
+        cell.configure(for: skylandersToDisplay[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        skylanderToSend = skylandersToDisplay[indexPath.row]
+        self.performSegue(withIdentifier: "DisplaySkylander", sender: Any?.self)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+//MARK: Search Bar Delegate
+extension SearchViewController:UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.searchText = searchText
+//        print(self.searchText)
+        tableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        tableView.reloadData()
     }
 }
